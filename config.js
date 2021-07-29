26193ee904f25da351ac41f335450958f8914bf3
Binnn0 <88153171+Binnn0@users.noreply.github.com>
2021-07-29 22:56:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Babe à! anh là Thịnh đây ',
+    introTitle: 'Babe à! anh là Thịnh đây :v ',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
